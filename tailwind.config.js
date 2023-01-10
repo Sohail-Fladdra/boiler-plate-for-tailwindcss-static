84eb675f166eb10504fa666a55553c8e6c391443
Sohail-Fladdra <sohail.ansari@fladdra.com>
2023-01-10 15:29:42 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './css/tailwind.css'],
+  theme: {
+    extend: {
+      colors: {
+        "custom-grey": "#E0E0E0",
+      },
+    },
+  },
+  plugins: [],
+};
